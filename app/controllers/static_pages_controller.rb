d06fc45c1d0966d63456dffd49db033df522b61f
keenanguyparker@gmail.com <valenciacurbello@Valencias-MacBook-Pro.local>
2016-05-25 22:43:45 -0700

    style comment section, reverse comment order in active migration, add all articles pathway, fix body header to page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
   	@posts = Post.last(4).reverse
-  	@comments = Comment.last(7)
+  	@comments = Comment.last(6).reverse
   	@comment = Comment.new
   end
 end
